fe5b88ec2de768babfdb527ff6ea93030c67240b
Dr. Gavin T.D. Greig <ggreig@ggreig.com>
2024-03-22 16:07:17 +0000

    Title: Add Api.Tests project and initial unit test Details: - Created a new .NET 8.0 test project named Api.Tests, intended to replace old style test project - Added NUnit and related packages for unit testing - Created a new UnitTests class with a setup method and a single passing test - Updated the solution file to include the new Api.Tests project

---
+++ Api.Tests/UnitTests.cs
@@ -0,0 +1,16 @@
+namespace Api.Tests
+{
+    public class UnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
