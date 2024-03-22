9099a06961afe1648af077a65f4b00f018c0209e
Dr. Gavin T.D. Greig <ggreig@ggreig.com>
2024-03-22 17:19:15 +0000

    Title: Added a test to ocnfirm controller returns expected results Description: - Added a new file `OrderControllerTests.cs` in the `Api.Tests/Controllers` directory. This file contains unit tests for the `OrderController`. - Added a project reference to `Api.csproj` in `Api.Tests.csproj`.

---
+++ Api.Tests/Controllers/OrderControllerTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Api.Tests.Controllers
+{
+    using Api.Controllers;
+    using FluentAssertions;
+    using Models;
+
+    [TestFixture]
+    [TestOf(typeof(OrderController))]
+    public class OrderControllerTests
+    {
+        [Test]
+        public void GetOrders_ShouldSucceed()
+        {
+            // Arrange
+            var orderController = new OrderController();
+
+            // Act
+            IEnumerable<Order> result = orderController.GetOrders();
+
+            // Assert
+            result.Should().HaveCount(3, "because 3 orders have been placed");
+        }
+    }
+}
