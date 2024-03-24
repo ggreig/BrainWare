986b9d9ec269f7d219f8046f1eccb92e788e8d2a
Dr. Gavin T.D. Greig <ggreig@ggreig.com>
2024-03-24 21:18:53 +0000

    Removed unused tests and turned on SQL Code Analysis

--- ProjectDB/Stored Procedures/getcompanyorders.sql
+++ ProjectDB/Stored Procedures/getcompanyorders.sql
@@ -2,6 +2,6 @@
 	@companyId int = 0
 AS
 	SELECT c.name, o.description, o.order_id 
-	FROM company c INNER JOIN [order] o on c.company_id=o.company_id
+	FROM Company c INNER JOIN [Order] o on c.company_id=o.company_id
 	WHERE c.company_id = @companyId
 RETURN 0
