2e9c472a4bec80f6ee47e3c46f9b4b149dfcbaf6
Dr. Gavin T.D. Greig <ggreig@ggreig.com>
2024-03-24 09:19:14 +0000

    Refactor: Organize order-related components and services into OrdersModule - Replaced import of OrderListComponent with OrdersModule in app.component.ts - Moved order-detail and order-list components into orders directory - Moved orders-service into orders directory - Created new OrdersModule and exported OrderListComponent

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { OrderListComponent } from './order-list/order-list.component';
+import { OrdersModule } from './orders/orders.module';
 import { Title } from '@angular/platform-browser';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule, OrderListComponent],
+  imports: [CommonModule, RouterModule, OrdersModule],
   selector: 'web-app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
